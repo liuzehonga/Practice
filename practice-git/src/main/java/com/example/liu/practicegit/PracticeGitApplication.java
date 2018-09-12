81adea53b6b3ad1ae0a264dff156c99d7d123990
liuzehong <calmnessliu@163.com>
2018-09-13 00:00:58 +0800

    one

---
+++ practice-git/src/main/java/com/example/liu/practicegit/PracticeGitApplication.java
@@ -0,0 +1,12 @@
+package com.example.liu.practicegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeGitApplication.class, args);
+    }
+}
