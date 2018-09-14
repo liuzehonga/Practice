11458ed4db8d7ed741b3c0397dcc55254ed679a8
liuzehong <calmnessliu@163.com>
2018-09-15 06:45:33 +0800

    ooo

--- practice-git/src/main/java/com/example/liu/practicegit/oneTest.java
+++ practice-git/src/main/java/com/example/liu/practicegit/oneTest.java
@@ -2,6 +2,6 @@ package com.example.liu.practicegit;
 
 public class oneTest {
     public static void main(String[] args) {
-        System.out.println("a,b");
+        System.out.println("a,b","c");
     }
 }
